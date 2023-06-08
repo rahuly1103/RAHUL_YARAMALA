7b54d0e4aa6e8287b5b1ea67eef57453ae4cfb6a
rahuly1103 <104550714+rahuly1103@users.noreply.github.com>
2023-06-08 18:48:04 +0530

    added a FAQ form folder

---
+++ FAQ form with basic html css and js/script.js
@@ -0,0 +1,17 @@
+const containers = document.querySelectorAll('.container');
+containers.forEach(container => {
+    const icon = container.querySelector('.icon');
+    const answer = container.querySelector('.answer');
+
+    container.addEventListener('click', ()=>{
+
+        if(icon.classList.contains('active')){
+            icon.classList.remove('active');
+            answer.style.maxHeight = null;
+        }
+        else{
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        }
+    })
+});
